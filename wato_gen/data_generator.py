adf134d9c33e9b0d4838e189b6a97f451724eaec
Eddy Zhou <eddy@sanctuary.ai>
2022-03-18 01:18:57 -0400

    writing generation...

--- wato_gen/data_generator.py
+++ wato_gen/data_generator.py
@@ -1,33 +1,65 @@
 import argparse
+import glob  
 import os
+dirname = os.path.dirname(os.path.abspath(__file__))
 
-def load_images_coco():
+from multiprocessing import Pool
+       
+
+def process_synth_data(data):
+
+
+  try:
+    pool.map(partial_func, params_list)
+  except KeyboardInterrupt:
+    print("....\nCaught KeyboardInterrupt, terminating workers")
+    pool.terminate()
+    pool.join()
 
   return
-
-def load_images_road_signs():
-  
-  return
-
-def generate_data():
-  
-  return
-
-def export():
-  
-  return
-
 
 def generate_road_sign_data(args):
   ''' Generate synthetic dataset according to given args
+      we assume that the number of augmented images is not bigger
+      than the coco training dataset (~581000 images)
   '''
-  # Load images from coco
-
-  # Load road sign data
+  # completing relative paths
+  coco_dir = os.path.join(dirname, args.coco)
+  road_sign_dir = os.path.join(dirname, args.road_signs)
   
-  # Generate data
+  # iterator for coco images and their labels
+  coco_image_iterator = glob.iglob(os.path.join(coco_dir, 'images', 'train2017', '*.jpg'))
+  coco_label_iterator = glob.iglob(os.path.join(coco_dir, 'labels', 'train2017', '*.txt'))
 
-  # Export data
+  # what we will be sending into the pool for multiprocessing
+  data_iterable = []
+
+  # for each of the road sign categories
+  for x in os.listdir(road_sign_dir):
+    road_sign_class = x
+
+    # iterator over the folder of augmented road_signs
+    road_sign_iterator = glob.iglob(os.path.join(road_sign_dir, road_sign_class, '*.png'))
+    finish_iteration = False
+
+    # iterate over all the augmented images in the folder 
+    while not finish_iteration:
+      try:
+        road_sign = next(road_sign_iterator)
+      except StopIteration:
+        finish_iteration = True
+      else:
+        data_iterable.append([road_sign, road_sign_class, 
+          next(coco_image_iterator), next(coco_label_iterator)])
+        
+        if len(data_iterable) is args.batch_size:
+          # send data_iterable in for processing
+          process_synth_data(data_iterable)
+
+          # empty data_iterable
+          data_iterable.clear()
+  
+  data_iterable.clear()
 
   return
 
@@ -36,16 +68,18 @@ def parse_args():
   '''
   parser = argparse.ArgumentParser(description="Creates a dataset of road signs")
   # Path Parameters
-  parser.add_argument("coco", default="/coco",
+  parser.add_argument("coco", default="coco",
     help="The coco directory which contains the images and labels.")
-  parser.add_argument("export", default="/wato_road_sign_data",
+  parser.add_argument("export", default="wato_road_sign_data",
     help="The directory where images and labels will be created.")
   parser.add_argument("road_signs", default="/mnt/wato-drive/perception_2d/signs_yolo/just_signs",
     help="WATonomous' road signs data.")
 
   # Generation Parameters
-  parser.add_argument("road_signs", default="/mnt/wato-drive/perception_2d/signs_yolo/just_signs",
-    help="WATonomous' road signs data.")
+  parser.add_argument("batch_size", default=64,
+    help="How many images do we want to process at a time?")
+  parser.add_argument("num_workers", default=4,
+    help="How many workers will be processing these images at a time? Each worker does batch_size/num_workers images")
   args = parser.parse_args()
 
   return args
@@ -53,4 +87,5 @@ def parse_args():
 if __name__ == '__main__':
   args = parse_args()
 
-  generate_road_sign_data(args)
+  with Pool(processes=args.num_workers) as pool:
+    generate_road_sign_data(args)
