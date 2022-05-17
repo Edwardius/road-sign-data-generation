c6525c3f3948c768b2adf75d8fd552c0904ba90c
Eddy Zhou <e23zhou@uwaterloo.ca>
2022-05-17 18:21:10 +0000

    different coco download script

--- wato_gen/get_coco_dataset.sh
+++ wato_gen/get_coco_dataset.sh
@@ -1,35 +1,27 @@
-mkdir coco
-cd coco
-mkdir images
-cd images
+#!/bin/bash
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+# Download COCO 2017 dataset http://cocodataset.org
+# Example usage: bash data/scripts/get_coco.sh
+# parent
+# ├── yolov5
+# └── datasets
+#     └── coco  ← downloads here
 
-wget http://images.cocodataset.org/zips/train2017.zip
-wget http://images.cocodataset.org/zips/val2017.zip
-wget http://images.cocodataset.org/zips/test2017.zip
-wget http://images.cocodataset.org/zips/unlabeled2017.zip
+# Download/unzip labels
+d='../datasets' # unzip directory
+url=https://github.com/ultralytics/yolov5/releases/download/v1.0/
+f='coco2017labels.zip' # or 'coco2017labels-segments.zip', 68 MB
+echo 'Downloading' $url$f ' ...'
+curl -L $url$f -o $f && unzip -q $f -d $d && rm $f &
 
-unzip train2017.zip
-unzip val2017.zip
-unzip test2017.zip
-unzip unlabeled2017.zip
-
-rm train2017.zip
-rm val2017.zip
-rm test2017.zip
-rm unlabeled2017.zip 
-
-cd ../
-wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
-wget http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip
-wget http://images.cocodataset.org/annotations/image_info_test2017.zip
-wget http://images.cocodataset.org/annotations/image_info_unlabeled2017.zip
-
-unzip annotations_trainval2017.zip
-unzip stuff_annotations_trainval2017.zip
-unzip image_info_test2017.zip
-unzip image_info_unlabeled2017.zip
-
-rm annotations_trainval2017.zip
-rm stuff_annotations_trainval2017.zip
-rm image_info_test2017.zip
-rm image_info_unlabeled2017.zip
+# Download/unzip images
+d='../datasets/coco/images' # unzip directory
+url=http://images.cocodataset.org/zips/
+f1='train2017.zip' # 19G, 118k images
+f2='val2017.zip'   # 1G, 5k images
+f3='test2017.zip'  # 7G, 41k images (optional)
+for f in $f1 $f2; do
+  echo 'Downloading' $url$f '...'
+  curl -L $url$f -o $f && unzip -q $f -d $d && rm $f &
+done
+wait # finish background tasks
